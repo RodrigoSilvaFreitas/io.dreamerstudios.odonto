4c4fae7475792b595ed050c383eb1800e38df37f
Rodrigo Freitas <rod.s.f@hotmail.com>
2023-07-12 23:58:52 -0300

    add migration

---
+++ Io.DreamerStudios.Odonto.Repository/Migrations/20230713025647_creating_table_people.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using MySql.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace Io.DreamerStudios.Odonto.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class creating_table_people : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterDatabase()
+                .Annotation("MySQL:Charset", "utf8mb4");
+
+            migrationBuilder.CreateTable(
+                name: "People",
+                columns: table => new
+                {
+                    Id = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("MySQL:ValueGenerationStrategy", MySQLValueGenerationStrategy.IdentityColumn),
+                    ResponsibleId = table.Column<long>(type: "bigint", nullable: true),
+                    Name = table.Column<string>(type: "longtext", nullable: false),
+                    Birthday = table.Column<DateTime>(type: "datetime(6)", nullable: false),
+                    Sex = table.Column<string>(type: "longtext", nullable: false),
+                    Document = table.Column<string>(type: "longtext", nullable: false),
+                    Phone = table.Column<string>(type: "longtext", nullable: false),
+                    CityId = table.Column<long>(type: "bigint", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_People", x => x.Id);
+                })
+                .Annotation("MySQL:Charset", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "People");
+        }
+    }
+}
